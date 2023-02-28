efcb5af2ca1160d9a84a1aed4a4f80bb40f2e653
alfi08 <alfisahri33@gmail.com>
2023-02-28 09:15:52 +0700

    Add validator.js

---
+++ src/validator.js
@@ -0,0 +1,11 @@
+// TODO: Lengkapi fungsi dibawah ini untuk memvalidasi terdapat data seperti full_name, email, dan phone_number
+const validateData = (data) => {
+
+}
+
+// TODO: Lengkapi fungsi dibawah ini untuk memvalidasi tidak terdapat data yang sama pada database
+const isDataDuplicated = (data) => {
+
+}
+
+module.exports = { validateData, isDataDuplicated }
